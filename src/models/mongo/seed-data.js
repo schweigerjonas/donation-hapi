c39dee3d957839475fa8abab8647ee5b6c800c91
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-17 08:42:04 +0100

    Update seed data

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -20,4 +20,44 @@ export const seedData = {
       password: "secret",
     },
   },
+  candidates: {
+    _model: "Candidate",
+    lisa: {
+      firstName: "Lisa",
+      lastName: "Simpson",
+      office: "President",
+    },
+    donald: {
+      firstName: "Donald",
+      lastName: "Simpson",
+      office: "President",
+    },
+  },
+  donations: {
+    _model: "Donation",
+    one: {
+      amount: 40,
+      method: "paypal",
+      donor: "bart@simpson.com",
+      candidate: "->candidates.lisa",
+      lat: "52.161290",
+      lng: "-7.51540",
+    },
+    two: {
+      amount: 90,
+      method: "direct",
+      donor: "marge@simpson.com",
+      candidate: "->candidates.lisa",
+      lat: "52.261290",
+      lng: "-7.231540",
+    },
+    three: {
+      amount: 430,
+      method: "paypal",
+      donor: "homer@simpson.com",
+      candidate: "->candidates.donald",
+      lat: "52.361290",
+      lng: "-7.241540",
+    },
+  },
 };
