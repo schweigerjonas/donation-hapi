6e0f507b0fe9088fc46b9545dd0b2adc2d14d94f
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-18 17:41:12 +0100

    Add types to route handlers

--- src/controllers/donations-controller.ts
+++ src/controllers/donations-controller.ts
@@ -1,18 +1,19 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
 import { db } from "../models/db.js";
 
 export const donationsController = {
   index: {
-    handler: async function (request, h) {
+    handler: async function (request: Request, h: ResponseToolkit) {
       const loggedInUser = request.auth.credentials;
       const candidates = await db.candidateStore.find();
       return h.view("donate", { title: "Make a Donation", user: loggedInUser, candidates: candidates });
     },
   },
   donate: {
-    handler: async function (request, h) {
+    handler: async function (request: Request, h: ResponseToolkit) {
       try {
         const loggedInUser = request.auth.credentials;
-        const donationPayload = request.payload;
+        const donationPayload = request.payload as any;
         const donation = {
           amount: donationPayload.amount,
           method: donationPayload.method,
@@ -24,13 +25,13 @@ export const donationsController = {
         await db.donationStore.add(donation);
 
         return h.redirect("/donate");
-      } catch (err) {
+      } catch (err: any) {
         return h.view("main", { errors: [{ message: err.message }] });
       }
     },
   },
   report: {
-    handler: async function (request, h) {
+    handler: async function (request: Request, h: ResponseToolkit) {
       const loggedInUser = request.auth.credentials;
       const donations = await db.donationStore.find();
       return h.view("report", {
