adccd7d1c3a9f48824f61b3921395b98c16ce80a
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-18 17:46:26 +0100

    Add remaining necessary type declarations

--- src/models/mongo/connect.ts
+++ src/models/mongo/connect.ts
@@ -1,10 +1,12 @@
 import * as dotenv from "dotenv";
 import Mongoose from "mongoose";
+// @ts-ignore
 import * as mongooseSeeder from "mais-mongoose-seeder";
 import { userStore } from "./user-store.js";
 import { candidateStore } from "./candidate-store.js";
 import { donationStore } from "./donation-store.js";
 import { seedData } from "./seed-data.js";
+import { Db } from "../../types/store-types.js";
 
 const seedLib = mongooseSeeder.default;
 
@@ -14,11 +16,11 @@ async function seed() {
   console.log(dbData);
 }
 
-export function connectMongo(db) {
+export function connectMongo(db: Db) {
   dotenv.config();
 
   Mongoose.set("strictQuery", true);
-  Mongoose.connect(process.env.DB);
+  Mongoose.connect(process.env.DB as string);
   const mongoDb = Mongoose.connection;
 
   db.userStore = userStore;
---
+++ src/types/store-types.ts
@@ -0,0 +1,5 @@
+export type Db = {
+  userStore: any;
+  candidateStore: any;
+  donationStore: any;
+};
